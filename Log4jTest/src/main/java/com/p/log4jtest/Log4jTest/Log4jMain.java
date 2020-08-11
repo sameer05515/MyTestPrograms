1c80ea3c6b54cafa4e87d857da470afc1efad75c
Sameer.05515 <sameer.geeee@gmail.com>
2020-08-11 18:50:49 +0530

    committing at Tue 08/11/2020 18:50:13.28

--- Log4jTest/src/main/java/com/p/log4jtest/Log4jTest/Log4jMain.java
+++ Log4jTest/src/main/java/com/p/log4jtest/Log4jTest/Log4jMain.java
@@ -1,19 +1,27 @@
 package com.p.log4jtest.Log4jTest;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 
 
 public class Log4jMain {
 	private final static Logger logger = Logger.getLogger(Log4jMain.class);
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws InterruptedException {
 		ExternalService extService = new ExternalService();
 		extService.setInput("JavaVillage");
 		extService.service();
-		logger.info("Response from External Service: "+extService.getOutput());
+		
 		
 		InternalService intService = new InternalService();
 		intService.setInput("Soha");
 		intService.service();
-		logger.info("Response from Internal Service: "+intService.getOutput());
+		while(true) {
+			for(int i=0;i<10000;i++) {
+				logger.info("Response from External Service : "+new Date()+" : "+extService.getOutput());
+				logger.info("Response from Internal Service : "+new Date()+" : "+intService.getOutput());
+			}
+			Thread.sleep(1000);
+		}
 	}
 }
