97937f385b6a748c2411c0997ab9373e5c87df3d
Sameer.05515 <sameer.geeee@gmail.com>
2020-09-08 22:22:05 +0530

    committing at Tue 09/08/2020 22:22:00.08

--- logback-with-springboot-config-master/src/main/java/com/lankydan/Application.java
+++ logback-with-springboot-config-master/src/main/java/com/lankydan/Application.java
@@ -1,12 +1,21 @@
 package com.lankydan;
 
 import com.lankydan.service.MyService;
+
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@EnableScheduling
 public class Application implements CommandLineRunner {
 
   @Autowired private MyService myService;
@@ -19,4 +28,12 @@ public class Application implements CommandLineRunner {
   public void run(String args[]) {
   	myService.doStuff("value");
 	}
+  
+  @Scheduled(cron = "${console.cronExpression}")
+	public void run() {
+//  	logger.info("info message : "+new Date());
+//  	logger.debug("debug message : "+new Date());
+//  	System.out.println("sysout message : "+new Date());
+	  myService.doStuff("value");
+  }
 }
