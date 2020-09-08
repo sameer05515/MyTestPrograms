584a2ed96afcce4bb407f42d83c0c7233d0fc72a
Sameer.05515 <sameer.geeee@gmail.com>
2020-09-08 20:20:05 +0530

    committing at Tue 09/08/2020 20:20:00.11

--- boot-logging-file-example/src/main/java/com/logicbig/example/ExampleMain.java
+++ boot-logging-file-example/src/main/java/com/logicbig/example/ExampleMain.java
@@ -3,9 +3,15 @@ package com.logicbig.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@EnableScheduling
 public class ExampleMain {
     private static final Logger logger = LoggerFactory.getLogger(ExampleMain.class);
 
