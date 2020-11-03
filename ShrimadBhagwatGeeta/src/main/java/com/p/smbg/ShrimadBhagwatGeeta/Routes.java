fbe36460ad76e02b963dd726d23cf91a32cf5ec6
Sameer.05515 <sameer.geeee@gmail.com>
2020-11-03 18:08:05 +0530

    committing at Tue 11/03/2020 18:07:14.44

---
+++ ShrimadBhagwatGeeta/src/main/java/com/p/smbg/ShrimadBhagwatGeeta/Routes.java
@@ -0,0 +1,23 @@
+package com.p.smbg.ShrimadBhagwatGeeta;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class Routes {
+
+    @RequestMapping({
+        "/"
+        // ,
+        // "/bikes",
+        // "/milages",
+        // "/gallery",
+        // "/tracks",
+        // "/tracks/{id:\\w+}",
+        // "/location",
+        // "/about"
+    })
+    public String index() {
+        return "forward:/index.html";
+    }
+}
---
+++ ShrimadBhagwatGeeta/src/test/java/com/p/smbg/ShrimadBhagwatGeeta/ShrimadBhagwatGeetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p.smbg.ShrimadBhagwatGeeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrimadBhagwatGeetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
