15815ebbcff4f075faaed2abcd0196516b34086b
Sameer05515 <sameer.geeee@gmail.com>
2020-11-24 22:45:57 +0530

    committing at 24-11-2020 22:45:11.44

--- MyTestPrograms/src/com/p/option/generation/StringTest.java
+++ MyTestPrograms/src/com/p/option/generation/StringTest.java
@@ -5,10 +5,14 @@ public class StringTest {
 	private static String query = "SELECT tc.cat_id,tc.cat_name,tc.creation_date,tc.last_updation_date,tc.rating,\r\n" + 
 			"(SELECT COUNT(*) FROM t_catg_ques tcq WHERE tcq.linked_cat_id=tc.cat_id) AS 'totalQuestionsCount'\r\n" + 
 			" FROM t_category tc ORDER BY last_updation_date DESC";
+	
+	private static String query2="select cat_id,cat_name,creation_date,last_updation_date,rating,last_read_date" +
+			", (SELECT COUNT(*) FROM t_catg_ques tcq WHERE tcq.linked_cat_id=cat_id) AS 'totalQuestionsCount'"
+			+ " from t_category" + " where cat_id=?";
 
 	public static void main(String[] args) {
 
-		System.out.println(query);
+		System.out.println(query2);
 	}
 
 }
