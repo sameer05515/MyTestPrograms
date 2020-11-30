d5657a743553f77adfdbb1e08bbda8bec4cdff8e
Sameer05515 <sameer.geeee@gmail.com>
2020-11-30 16:07:35 +0530

    committing at 30-11-2020 16:06:57.27

---
+++ ParentChildRelationsTopics/src/main/java/com/p/parent/child/relation/topics/ParentChildRelationsTopics/ParentChildRelationsTopicsApplication.java
@@ -0,0 +1,13 @@
+package com.p.parent.child.relation.topics.ParentChildRelationsTopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentChildRelationsTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentChildRelationsTopicsApplication.class, args);
+	}
+
+}
---
+++ ParentChildRelationsTopics/src/test/java/com/p/parent/child/relation/topics/ParentChildRelationsTopics/ParentChildRelationsTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p.parent.child.relation.topics.ParentChildRelationsTopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentChildRelationsTopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
