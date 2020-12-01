7bfdd22a37b5d24bc6d2b59d246dd78aa46e7a87
Sameer05515 <sameer.geeee@gmail.com>
2020-12-02 02:44:55 +0530

    committing at 02-12-2020  2:44:22.10

--- coding.ex/src/main/java/com/empl/coding/exercise/coding/ex/controller/EmployeeController.java
+++ coding.ex/src/main/java/com/empl/coding/exercise/coding/ex/controller/EmployeeController.java
@@ -1,19 +1,42 @@
 package com.empl.coding.exercise.coding.ex.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.empl.coding.exercise.coding.ex.pojo.Employee;
+
 @RestController
 public class EmployeeController {
-	
+
 	@GetMapping("/")
 	public String healthCheck() {
 		return "OK";
 	}
-	
-	@PutMapping("/employee/place/{place}/salary/{percentage}")
-	public List<Empl>
-	
 
+	@PutMapping("/employee/place/{place}/salary/{percentage}")
+	public int incrementSalaryOfGivenPlaceEmployees(@PathVariable("place") String place,
+			@PathVariable("percentage") int percentage) {
+
+		return 0;
+	}
+
+	@GetMapping("/employee/place/{place}")
+	public List<Employee> getEmployeesHavingPlace(@PathVariable("place") String place) {
+		List<Employee> res = new ArrayList<Employee>();
+
+		return res;
+	}
+
+	@GetMapping("/employee/competency/{competency}/salary/range")
+	public JSONObject getRangeOfSalaryHavingCompetency(@PathVariable("competency") String competency) {
+		JSONObject res = new JSONObject();
+
+		return res;
+	}
 }
--- coding.ex/src/main/java/com/empl/coding/exercise/coding/ex/pojo/Employee.java
+++ coding.ex/src/main/java/com/empl/coding/exercise/coding/ex/pojo/Employee.java
@@ -7,105 +7,117 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.json.JSONObject;
+
 @Entity
 @Table(name = "t_employee")
 public class Employee {
 
 	@Id
 	@GeneratedValue
-	int id;
-	String unique_name;
-	String word;
-	String type;
-	String details;
-	Date created_on;
-	Date updated_on;
-	Date last_read;
+	int employeeID;
+	String employeeName;
+	String title;
+	String businessUnit;
+	String place;
+	int supervisorID;
+	String competencies;
+	int salary;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUnique_name() {
-		return unique_name;
-	}
-
-	public void setUnique_name(String unique_name) {
-		this.unique_name = unique_name;
-	}
-
-	public String getWord() {
-		return word;
-	}
-
-	public void setWord(String word) {
-		this.word = word;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getDetails() {
-		return details;
-	}
-
-	public void setDetails(String details) {
-		this.details = details;
-	}
-
-	public Date getCreated_on() {
-		return created_on;
-	}
-
-	public void setCreated_on(Date created_on) {
-		this.created_on = created_on;
-	}
-
-	public Date getUpdated_on() {
-		return updated_on;
-	}
-
-	public void setUpdated_on(Date updated_on) {
-		this.updated_on = updated_on;
-	}
-
-	public Date getLast_read() {
-		return last_read;
-	}
-
-	public void setLast_read(Date last_read) {
-		this.last_read = last_read;
-	}
-
+	
 	@Override
 	public String toString() {
-//		JSONObject obJsonObject=new JSONObject();
-//		obJsonObject.put("id", id);
-//		obJsonObject.put("unique_name", unique_name);
-//		obJsonObject.put("word", word);
-//		obJsonObject.put("type", type);
-//		obJsonObject.put("details", details);
-//		obJsonObject.put("created_on", created_on);
-//		obJsonObject.put("updated_on", updated_on);
-//		obJsonObject.put("last_read", last_read);
-//		
-//		return obJsonObject.toJSONString();
+		JSONObject obJsonObject=new JSONObject();		
+		obJsonObject.put("employeeID", employeeID);
+		obJsonObject.put("employeeName", employeeName);
+		obJsonObject.put("title", title);
+		obJsonObject.put("businessUnit", businessUnit);
+		obJsonObject.put("place", place);
+		obJsonObject.put("supervisorID", supervisorID);
+		obJsonObject.put("competencies", competencies);
+		obJsonObject.put("salary", salary);
 		
-		
-		
-		
-		return "\n{\"id\":\"" + id + "\", \"unique_name\":\"" + unique_name + "\", \"word\":\"" + word + "\", \"type\":\"" + type
-				+ "\", \"details\":\"" + details + "\", \"created_on\":\"" + created_on + "\", \"updated_on\":\"" + updated_on
-				+ "\", \"last_read\":\"" + last_read + "\"}";
+		return obJsonObject.toString();
+	}
+
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getBusinessUnit() {
+		return businessUnit;
+	}
+
+
+	public void setBusinessUnit(String businessUnit) {
+		this.businessUnit = businessUnit;
+	}
+
+
+	public String getPlace() {
+		return place;
+	}
+
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+
+	public int getSupervisorID() {
+		return supervisorID;
+	}
+
+
+	public void setSupervisorID(int supervisorID) {
+		this.supervisorID = supervisorID;
+	}
+
+
+	public String getCompetencies() {
+		return competencies;
+	}
+
+
+	public void setCompetencies(String competencies) {
+		this.competencies = competencies;
+	}
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
 	}
 
 }
