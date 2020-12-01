3b8b0229efb467faff514b41e26fef8c20edfb3a
Premendra.Kumar <premendra.bce05515@gmail.com>
2020-12-01 19:59:10 +0530

    coding-example-assigned by bglr recruiter

---
+++ coding.ex/src/test/java/com/empl/coding/exercise/coding/ex/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empl.coding.exercise.coding.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
