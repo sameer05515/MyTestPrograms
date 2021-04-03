8946fac870487fa21e548aa8cfb3e4bf86f25936
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-04-03 17:33:54 +0530

    committing at Saturday 03 April 2021 05:33:35 PM IST

--- Resume/src/main/java/com/p/resume/controller/ResumeController.java
+++ Resume/src/main/java/com/p/resume/controller/ResumeController.java
@@ -1,33 +1,35 @@
 package com.p.resume.controller;
 
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.p.resume.exception.InvalidInputSuppliedException;
 import com.p.resume.response.ResponseHandler;
 import com.p.resume.service.ResumeService;
 
+@RestController
 public class ResumeController {
 	
 	@Autowired
-	ResumeService employeeService;
+	ResumeService resumeService;
 
 	@GetMapping("/")
 	public String healthCheck() {
 		return "OK";
 	}
 	
-	@PutMapping("/employee/place/{place}/salary/{percentage}")
-	public ResponseEntity<Object> incrementSalaryOfGivenPlaceEmployees(@PathVariable("place") String place,
-			@PathVariable("percentage") int percentage) {
+	@GetMapping("/user/{id}")
+	public ResponseEntity<Object> getUserDetails(@PathVariable("id") int id) {
 		ResponseEntity<Object> response=null;
 		try {
-			int res=employeeService.incrementSalaryOfGivenPlaceEmployees(place,percentage);
+			Map<String, Object> res=resumeService.getUserDetails(id);
 			response = ResponseHandler.generateResponse(HttpStatus.OK, false, "Success", res);
 		} catch (InvalidInputSuppliedException e) {
 			response = ResponseHandler.generateResponse(HttpStatus.PRECONDITION_FAILED, true, "Fail",
