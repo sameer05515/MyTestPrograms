9f048f919761b545c00e5a5edbac640d4eda1e3e
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-06-19 13:44:48 +0530

    committing at Saturday 19 June 2021 01:44:47 PM IST

--- MyTestPrograms/src/com/p/pers/calldetail/CallDetailsQuery.java
+++ MyTestPrograms/src/com/p/pers/calldetail/CallDetailsQuery.java
@@ -4,5 +4,9 @@ public final class CallDetailsQuery {
 
 	public static String getDistinctNumberWithPulses = "SELECT DISTINCT number, sum(pulses),month FROM t_call_details \n"
 			+ "GROUP BY number,month\n" + "ORDER BY month,sum(pulses) DESC";
+	
+	public static void main(String[] args) {
+		System.out.println(getDistinctNumberWithPulses);
+	}
 
 }
