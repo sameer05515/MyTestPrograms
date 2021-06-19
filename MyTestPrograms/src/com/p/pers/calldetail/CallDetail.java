5250cb5546673cb5e3827e0ba7fc2abfc493ebf9
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-06-19 14:04:47 +0530

    committing at Saturday 19 June 2021 02:04:46 PM IST

--- MyTestPrograms/src/com/p/pers/calldetail/CallDetail.java
+++ MyTestPrograms/src/com/p/pers/calldetail/CallDetail.java
@@ -15,26 +15,28 @@ public class CallDetail {
 
 	public static void main(String[] args) {
 		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss"); //  04/02/2011 20:27:05
+			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //  04/02/2011 20:27:05
+			
 			List<String> rawDataList = getRawList("/home/premendra/Desktop/1/june-21/v-call-june21.txt");
 			
 			String name="Vandana Kumari";
 			String billCycle="May 2021 bill";
 			if (rawDataList != null && rawDataList.size() > 0) {
 
-				Set<String> numbers = new HashSet<String>();
+//				Set<String> numbers = new HashSet<String>();
 
 				for (String line : rawDataList) {
 					if (line != null && !line.trim().equalsIgnoreCase("")) {
 						String[] tokens = line.split(" ");
 						int i = 0;
-						numbers.add(tokens[3]);
+//						numbers.add(tokens[3]);
 						
 						////////////////////////
 						//2021-06-19 12:56:18
 						// 08/APR/2021 13:16:43
 						
-						SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss"); //  04/02/2011 20:27:05
-						SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //  04/02/2011 20:27:05
+						
 						
 				        Date date = sdf.parse(tokens[1]+" "+tokens[2]); // returns date object
 //				        System.out.println(date); // outputs: Fri Feb 04 20:27:05 IST 2011
@@ -44,10 +46,10 @@ public class CallDetail {
 				        		"('%s', '%s', '%s','%s','%s');\n",name,sdf1.format(date),tokens[3],tokens[5],billCycle);
 				        ////////////////////////
 						
-						for (String t : tokens) {
+//						for (String t : tokens) {
 
 //							System.out.print("\t token[" + (i++) + "] == " + t);
-						}
+//						}
 //						System.out.println();
 					}
 				}
